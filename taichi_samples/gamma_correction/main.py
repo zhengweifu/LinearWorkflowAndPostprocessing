47ca6976465984b9ce67e1cb36cefff91015cdc2
郑卫福 <651999307@qq.com>
2023-12-17 12:30:02 +0800

    tonemapping 和 gammacorrection 加入GUI控制

--- taichi_samples/gamma_correction/main.py
+++ taichi_samples/gamma_correction/main.py
@@ -11,19 +11,25 @@ Vector3 = ti.types.vector(n=3, dtype=ti.f32)
 def GammaCorrection(color):
     return ti.math.pow(color, 0.454545)
 
+@ti.func
+def InvGammaCorrection(color):
+    return ti.math.pow(color, 2.2)
 
 @ti.kernel
-def GammaCorrectionKernel(img: Img2d):
+def GammaCorrectionKernel(img: Img2d, bInvGC: ti.u8):
     h, w = img.shape[0], img.shape[1]
     for i, j in ti.ndrange(h, w):
         c = Vector3(img[i, j] / 255.0)
-        img[i, j] = (GammaCorrection(c) * 255.0).cast(ti.u8)
+        if bInvGC > ti.u8(0):
+            img[i, j] = (InvGammaCorrection(c) * 255.0).cast(ti.u8)
+        else:
+            img[i, j] = (GammaCorrection(c) * 255.0).cast(ti.u8)
 
 def Main():
     imgPath = os.path.join(os.getcwd(), "images/sh_smaller.png")
     img = cv2.imread(imgPath, cv2.IMREAD_UNCHANGED)
     
-    GammaCorrectionKernel(img)
+    GammaCorrectionKernel(img, 1)
 
     titleName = "Gamma Correction"
     cv2.imshow(titleName, img)
--- taichi_samples/global_tonemapping/main.py
+++ taichi_samples/global_tonemapping/main.py
@@ -17,51 +17,63 @@ Vector3 = ti.types.vector(n=3, dtype=ti.f32)
 def GammaCorrection(color):
     return ti.math.pow(color, 0.454545)
 
-# 0->ReinhardToneMapping, 
-# 1->CryEngineToneMapping, 
-# 2->FilmicToneMapping, 
-# 3->ACESToneMapping,
-# 4->AdvanceACESToneMapping
-tonemapingType = 4
-
 @ti.kernel
-def ToneMapping(img: Img2d):
+def ToneMapping(img: Img2d, tonemapingType: ti.u8):
     h, w = img.shape[0], img.shape[1]
     halfW = w * 0.5
     halfH = h * 0.5
     for i, j in ti.ndrange(h, w):
         c = Vector3(img[i, j])
-        if tonemapingType == 0:
+        if tonemapingType == 1:
             img[i, j] = ReinhardToneMapping(c, 1.0)
-        elif tonemapingType == 1:
-            img[i, j] = CryEngineToneMapping(c, 1.0)
         elif tonemapingType == 2:
-            img[i, j] = FilmicToneMapping(c, 1.0)
+            img[i, j] = CryEngineToneMapping(c, 1.0)
         elif tonemapingType == 3:
-            img[i, j] = ACESToneMapping(c, 1.0)
+            img[i, j] = FilmicToneMapping(c, 1.0)
         elif tonemapingType == 4:
+            img[i, j] = ACESToneMapping(c, 1.0)
+        elif tonemapingType == 5:
             img[i, j] = AdvanceACESToneMapping(c, 1.0)
         img[i, j] = GammaCorrection(img[i, j])
 
 def Main():
     imgPath = os.path.join(os.getcwd(), "images/veranda_1k.hdr")
     img = cv2.imread(imgPath, cv2.IMREAD_UNCHANGED)
-    
-    ToneMapping(img)
+    srcImg = img.swapaxes(0, 1)[:, ::-1, ::-1].copy()
 
-    titleName = "No tonemapping"
-    if tonemapingType == 0:
-        titleName = "Reinhard tonemapping"
-    elif tonemapingType == 1:
-        titleName = "CryEngine tonemapping"
-    elif tonemapingType == 2:
-        titleName = "Filmic tonemapping"
-    elif tonemapingType == 3:
-        titleName = "ACES tonemapping"
-    elif tonemapingType == 4:
-        titleName = "ACES tonemapping advance"
-    cv2.imshow(titleName, img)
-    cv2.waitKey()
+    titleName = "Tone Mapping"
+    window = ti.ui.Window(titleName, res=(srcImg.shape[0], srcImg.shape[1]), fps_limit=200, pos = (150, 150))
+
+    tonemapingType = 0
+    tonemapingTypeName = "No tonemapping"
+    while window.running:
+        window.GUI.begin("Tone Mapping", 0.0, 0.0, 0.2, 0.2)
+        if tonemapingType == 0:
+            tonemapingTypeName = "No tonemapping"
+        elif tonemapingType == 1:
+            tonemapingTypeName = "Reinhard tonemapping"
+        elif tonemapingType == 2:
+            tonemapingTypeName = "CryEngine tonemapping"
+        elif tonemapingType == 3:
+            tonemapingTypeName = "Filmic tonemapping"
+        elif tonemapingType == 4:
+            groutonemapingTypeNamepName = "ACES tonemapping"
+        elif tonemapingType == 5:
+            tonemapingTypeName = "ACES tonemapping advance"
+        window.GUI.text(tonemapingTypeName)
+        tonemapingType = window.GUI.slider_int('', tonemapingType, minimum=0, maximum=5)
+        window.GUI.end()
+
+        img = srcImg.copy()
+
+        ToneMapping(img, tonemapingType)
+
+        uiShape = window.get_window_shape()
+        img_padded = np.zeros(dtype=np.float32, shape=(uiShape[0], uiShape[1], 3))
+        img_padded[:img.shape[0], :img.shape[1]] = img
+        window.get_canvas().set_image(img_padded)
+
+        window.show()
 
 if __name__ == "__main__":
     Main()
